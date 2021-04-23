bbe7946eeb487273b6f460af8beb94618185a0aa
mads-br <52624217+mads-br@users.noreply.github.com>
2021-04-23 13:34:06 +0200

    New baseline model

--- src/model.py
+++ src/model.py
@@ -1,3 +1,28 @@
-# Very simple baseline model
-def baseline_model(fasta_sequence: str):
-    return (6 + len(fasta_sequence)) / 1000 + 7
+from sklearn import tree
+import numpy as np
+import pickle
+
+
+class BaselineModel:
+
+    def vectorize_sequences(self, sequence_array):
+        vectorize_on_length = np.vectorize(len)
+        return np.reshape(vectorize_on_length(sequence_array), (-1, 1))
+
+    def train(self, df_train, model_save_path):
+        X = self.vectorize_sequences(df_train['sequence'].to_numpy())
+        y = df_train['mean_growth_PH'].to_numpy()
+
+        model = tree.DecisionTreeRegressor()
+        model.fit(X, y)
+
+        with open(model_save_path, 'wb') as model_file:
+            pickle.dump(model, model_file)
+
+    def predict(self, df_test, model_file_path):
+        with open(model_file_path, 'rb') as model_file:
+            model: tree.DecisionTreeRegressor = pickle.load(model_file)
+
+        X = df_test['sequence'].to_numpy()
+        X_vectorized = self.vectorize_sequences(X)
+        return model.predict(X_vectorized)
--- src/predict.py
+++ src/predict.py
@@ -1,28 +1,24 @@
 import argparse
 import pandas as pd
-
-from model import baseline_model
+from model import BaselineModel
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--input_csv', default='input.csv')
 args = parser.parse_args()
 
 # Config
-model_to_use = baseline_model
+model_file_path = 'model.pickle'
 output_file_path = 'predictions.csv'
 
 # Load input.csv
 with open(args.input_csv) as input_csv:
-    data_frame = pd.read_csv(input_csv)
+    df = pd.read_csv(input_csv)
 
 # Run predictions
-Y_predictions = []
-for index, row in data_frame.iterrows():
-    prediction = model_to_use(row['sequence'])
-    Y_predictions.append(prediction)
+y_predictions = BaselineModel().predict(df, model_file_path)
 
 # Save predictions to file
-df_predictions = pd.DataFrame({'prediction': Y_predictions})
+df_predictions = pd.DataFrame({'prediction': y_predictions})
 df_predictions.to_csv(output_file_path, index=False)
 
-print(f'{len(Y_predictions)} predictions saved to a csv file')
+print(f'{len(y_predictions)} predictions saved to a csv file')
